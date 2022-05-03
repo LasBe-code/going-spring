e992155d51491bf8dfaa8b1e6ffbb5aa0ba5c4b8
lasbe <metoo0339@naver.com>
2022-05-03 17:20:17 +0900

    Admin 기능 java 파일 생성

---
+++ src/main/java/mybatis/AdminMapperAnno.java
@@ -0,0 +1,5 @@
+package mybatis;
+
+public interface AdminMapperAnno {
+
+}
---
+++ src/main/java/repository/AdminDao.java
@@ -0,0 +1,8 @@
+package repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AdminDao {
+
+}
